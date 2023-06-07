6c7a01fe45864e65b1204bedaaa6720fe0853aa0
Rajat Tyagi <tyagirajat200@gmail.com>
2023-06-07 10:29:41 +0000

    Sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Observable, fromEvent } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { debounceTime, map, mergeMap, switchMap } from 'rxjs/operators';
+import { DataService } from './data.service';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular 13';
+
+  @ViewChild('search', { static: true }) search!: ElementRef;
+
+  states: any[] = [];
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    fromEvent(this.search.nativeElement, 'input')
+      .pipe(
+        map((event: any) => event.target.value),
+        debounceTime(500),
+        switchMap((searchText: string) => this.dataService.getData(searchText))
+      )
+      .subscribe((res) => {
+        this.states = res;
+        console.log(res)
+      });
+  }
+
 }
